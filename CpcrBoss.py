7e4cb457f30b8b72d6f82f1f045f942a9727b476
admin-PC\admin <540383428@qq.com>
2017-12-20 14:00:39 +0800

    Signed-off-by: admin-PC\admin <540383428@qq.com>

--- CpcrBoss.py
+++ CpcrBoss.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# python -version = 2.7
 # Todo：接口自动化测试
 # Author：赵华兵
 # Data : 2017-12-7
-
 import json,requests
-
 import time
 import re
 import logging
@@ -22,9 +19,9 @@ except:
     os.system('pip install -U xlrd')
     import xlrd
 try:
-    from pyDes import *
+    import requests
 except ImportError as e:
-    os.system('pip install -U pyDes --allow-external pyDes --allow-unverified pyDes')
+    os.system('pip install -U requests')
     from pyDes import *
 import smtplib  
 from email.mime.text import MIMEText 
@@ -50,8 +47,8 @@ ch.setFormatter(formatter)
 # 第五步，将logger添加到handler里面
 logger.addHandler(fh)
 logger.addHandler(ch)
-errorCase = []
 #获取并执行测试用例
+errorCase = []
 def runTest(testCaseFile):
     '''
     作者：赵华兵
@@ -62,7 +59,7 @@ def runTest(testCaseFile):
     if not os.path.exists(testCaseFile):
         logging.error('测试用例文件不存在！！！')
         sys.exit()
-    testCase = xlrd.open_workbook(testCaseFile)
+    testCase = xlrd.open_workbook(testCaseFile,encoding_override="cp1252")
     table = testCase.sheet_by_index(0)
 
     correlationDict = {}
@@ -76,7 +73,7 @@ def runTest(testCaseFile):
         request_method = table.cell(i,4).value.replace('\n','').replace('\r','').encode('utf-8')
         request_data_type = table.cell(i,5).value.replace('\n','').replace('\r','')
         request_data = table.cell(i,6).value.replace('\n','').replace('\r','')
-        encryption = table.cell(i,7).value.replace('\n','').replace('\r','')
+        Category = table.cell(i,7).value.replace('\n','').replace('\r','').encode('utf-8')
         check_point = table.cell(i,8).value.encode('utf-8')#replace('\n','').replace('\r','')
         correlation = table.cell(i,9).value.replace('\n','').replace('\r','').split(';')
 
@@ -84,11 +81,10 @@ def runTest(testCaseFile):
             ''' 将xlsx中request_data 'unicode'编码转化成 dict类型'''
             request_data_new=json.loads(request_data.encode('utf-8'))
 
-        status, response = interfaceTest(num=num,api_purpose=api_purpose,api_host=api_host, request_url=request_url, request_data=request_data_new,
-                               request_method=request_method, check_point=check_point,request_data_type=request_data_type)
-
+        status, response = interfaceTest(num=num,api_purpose=api_purpose,api_host=api_host, request_url=request_url, request_data=request_data_new,request_method=request_method, check_point=check_point,request_data_type=request_data_type,Category=Category,correlation=correlation)
         if status != 200:
-            errorCase.append((num + ' ' + api_purpose, str(status), 'http://' + api_host + request_url, response))
+            check_point_str='无'
+            errorCase.append((num + ' ' + api_purpose, str(status), 'http://' + api_host + request_url,check_point_str+"," +'请求参数='+request_data.encode('utf-8'),response))
 #         for key in correlationDict:
 #             if request_url.find(key) > 0:
 #                 request_url = request_url.replace(key,str(correlationDict[key]))
@@ -155,48 +151,88 @@ def runTest(testCaseFile):
 #                 correlationDict[param[0]] = value
     return errorCase
 
+
 # 接口测试
-def interfaceTest(num,api_purpose,api_host,request_url,request_data,check_point,request_method,request_data_type):
+def interfaceTest(num,api_purpose,api_host,request_url,request_data,check_point,request_method,request_data_type,Category,correlation):
     '''
     作者：赵华兵
     时间: 2017-12-7
     功能: 实现http接口请求方法，参数==>> 和.xlsx表格数据对应!
     '''
-    headers = {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-               'X-Requested-With':'XMLHttpRequest',
-               'Connection':'keep-alive',
-               'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36'}
-    request_url_new = 'http://' + api_host + request_url
-    if request_method == 'POST':
-        rq = requests.post(request_url_new, request_data, headers=headers)
+    #截取cookie
+    cookie_list=[]
+    if Category == "boss":
+        cookie_value=cookie("boss")
+        cookie_list.append(cookie_value)
+    elif Category == "mch":
+        cookie_value = cookie("mch")
+        cookie_list.append(cookie_value)
+    if len(cookie_list) != 0:
+        Cookie = "X-Nova-Token=%s"%cookie_list[0]
+    else:
+        Cookie =""
 
+    headers = {"Content-Type":"application/json",
+               'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
+               'Cookie': Cookie,
+               }
+    request_url_new = 'http://' + api_host + request_url
+    data=json.dumps(request_data)
+
+    if request_method == "POST":
+        response_info = requests.post(url=request_url_new,data=data,headers=headers)
+    elif request_method == "GET":
+        response_info = requests.get(url=request_url_new,headers=headers)
     else:
         logging.error(num + ' ' + api_purpose + ' HTTP请求方法错误，请确认[Request Method]字段是否正确！！！')
-        return 400,request_method
-    response =rq.text
-    status = rq.status_code
+    response = response_info.text
+    status = response_info.status_code
+    #状态码判断
     if status == 200:
         resp = response.encode('utf-8')
         if re.search(check_point,resp):
             logging.info(num + ' ' + api_purpose + ' 成功, ' + str(status) + ', '+resp)
             return status,json.loads(resp)
         else:
-            logging.error(num + ' ' + api_purpose + ' 失败！！！, [ ' + str(status) + ' ], ' + resp)
-            errorCase.append((num + ' ' + api_purpose, str(status), 'http://' + api_host + request_url, response))
+            logging.error(num + ' ' + api_purpose + ' 失败！！！, [ ' + str(status) + ' ], '+ resp)
+            errorCase.append((num + ' ' + api_purpose, str(status), 'http://' + api_host + request_url,check_point +"," +'请求参数='+str(request_data).encode('utf-8') ,response))
             return status,resp
     else:
         logging.error(num + ' ' + api_purpose + ' 失败！！！, [ ' + str(status) + ' ], ' + response)
         return status,response
+    return
 
+def cookie(Category):
+    '''
+        作者：赵华兵
+        时间: 2017-12-11
+        功能: 获取对应的cookie信息!  boss =运营中心， mch=商户中心
+        '''
+    headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+               'X-Requested-With': 'XMLHttpRequest',
+               'Connection': 'keep-alive',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36'
+               }
+    if Category == "boss":
+        url='http://cpcr.boss.novaszco.com/api/user/login'
+        data = {"username": "admin001",
+                "password": "admin001"}
+        cookie_response = requests.post(url=url, headers=headers, data=data)
 
-    return #status, response
+    elif Category =="mch":
+        url = 'http://cpcr.mch.novaszco.com/api/user/login'
+        data = {"username": "admin001",
+                "password": "admin001"}
+        cookie_response = requests.post(url=url, headers=headers, data=data)
 
+    return cookie_response.cookies.get_dict()['X-Nova-Token']
 
 #发送通知邮件
 def sendMail(text):
     sender = '13823720073@163.com'
     #收件人邮箱，，可设置多个
     receiver = ['13823720073@163.com']
+    # 抄送人
     mailToCc = ['13823720073@163.com']
     subject = '[AutomantionTest]接口自动化测试报告通知'  
     smtpserver = 'smtp.163.com'
@@ -214,25 +250,16 @@ def sendMail(text):
     smtp.sendmail(sender, receiver + mailToCc, msg.as_string())  
     smtp.quit() 
 
-def main():
-    errorTest = runTest('TestCase/TestCase.xlsx')
+def main(Casepath):
+    errorTest = runTest(Casepath)
 
-    if len(errorTest) >= 0:
-        html = '<html><body><p>接口自动化定期扫描，共有 ' + str(len(errorTest)) + ' 个异常接口，列表如下：' + '</p><table style="border: 1px solid black;"><tr style="background: green;"><th style="width:100px;">接口编号</th><th style="width:50px;">状态</th><th style="width:200px;">接口地址</th><th">接口返回值</th></tr>'
+    if len(errorTest) > 0:
+
+        html = '<html><body><p>接口自动化定期扫描，共有 ' + str(len(errorTest)) + ' 个异常接口，列表如下：' + '</p><table style="border: 1px solid black;"><tr style="background: green;"><th style="width:100px;">接口编号</th><th style="width:50px;">状态</th><th style="width:200px;">接口地址</th><th style="width:50px;">断言及校验参数</th><th thstyle="width:50px;">返回结果</th></tr>'
         for test in errorTest:
-            html = html + '<tr style="background: grey"><td style="padding: 10px;"><h5>' + test[0] + '</td></h5><td style="padding: 10px;">' + test[1] + '</td><td style="padding: 10px;">' + test[2] + '</td><td style="padding: 10px;">' + test[3] + '</td></tr>'
+            # print test
+            html = html + '<tr style="background: grey"><td style="padding: 10px;"><h5>' + test[0] + '</td></h5><td style="padding: 10px;">' + test[1] + '</td><td style="padding: 10px;">' + test[2] + '</td><td style="padding: 10px;">' + test[3] + '</td><td style="padding: 10px;">' + test[4] + '</td></tr>'
         html = html + '</table></body></html>'
         sendMail(html)
 
-if __name__ == '__main__':
-    main()
-# if __name__ =='__main__':
-# #     api_host="cpel.novaszco.com"
-# #     request_url="/api/user/login"
-# #     request_data={"username":"adminZH","password":"test001"}
-# #     request_method="POST"
-# #     check_point="SUCCESS"
-# #
-# #     start,sep=interfaceTest(api_host=api_host,request_url=request_url,request_data=request_data,request_method=request_method,check_point=check_point)
-# #     print start,sep
-#     runTest('TestCase/TestCase.xlsx')
+
---
+++ run.py
@@ -0,0 +1,4 @@
+from CpcrBoss import main
+
+if __name__ == "__main__":
+    main('TestCase/cpcrBossTestCase.xlsx')
